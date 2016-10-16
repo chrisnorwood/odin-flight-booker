79c27e9edddde0513c601d372b365280a615c7fb
Chris Norwood <cnorwood92@gmail.com>
2016-10-15 23:56:54 -0700

    Change email port

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :addresses      => 'smtp.sendgrid.net',
-    :port           => '465',
+    :port           => 587,
     :authentication => :plain,
     :user_name      => Figaro.env.sendgrid_username,
     :password       => Figaro.env.sendgrid_password,
